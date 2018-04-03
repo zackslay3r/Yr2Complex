fb63774ab59180bd2c0b4540b94fd47b4ab909b7
Zackary Direen <zackary.direen@hotmail.com>
2018-04-03 16:07:50 +1000

    connection checks

--- Testing/Project1/Main.cpp
+++ Testing/Project1/Main.cpp
@@ -3,8 +3,31 @@
 #include <string>
 int main()
 {
+	bool validPort = false;
+	short SelectedPortNumber;
 	Client* newClient = new Client;
-	newClient->ClientStuff(sf::IpAddress::getLocalAddress().toString(), 2501);
+
+	while (!validPort)
+	{
+		std::cout << "Insert a number between 1-5000 as the port to connect to.";
+		std::cin >> SelectedPortNumber;
+		if (SelectedPortNumber > 1 || SelectedPortNumber < 5000)
+		{
+			if (newClient->ClientStuff(sf::IpAddress::getLocalAddress().toString(), SelectedPortNumber))
+			{
+				validPort == true;
+				std::cout << "Connected established.";
+			}
+			else
+			{
+				std::cout << "COnnection failed. Invalid Port.";
+			}
+			
+		}
+	
+	}
+
+	
 	system("pause");
 	return 0;
 }
--- Testing/Server/main.cpp
+++ Testing/Server/main.cpp
@@ -4,13 +4,29 @@
 
 int main()
 {
-
+	bool validPort = false;
 	std::thread ListenerThread;
 	Server* s = new Server;
 
 	sf::Thread* thread = 0;
 
-	s->PORT = 2501;
+
+	while (validPort = false)
+	{
+		std::cout << "Please put a number in between 1 and 5000 for a port number.";
+		short tempShort;
+		std::cin >> tempShort;
+		if (tempShort > 0 || tempShort < 5000)
+		{
+			s->PORT = tempShort;
+			validPort = true;
+		}
+
+	}
+
+
+
+	//s->PORT = 2501;
 	// this is a function pointer
 	// function as a variable.
 	ListenerThread = std::thread([s] {s->ServerStuff(); });
